8cb06f58e2efb0d1b55abea250c6bcd2d19c36a0
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-08-25 13:42:15 -0400

    Begin documentation of initial top-level CLI commands

--- cmd/authenticate.go
+++ cmd/authenticate.go
@@ -1,17 +1,3 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -22,14 +8,9 @@ import (
 
 // authenticateCmd represents the authenticate command
 var authenticateCmd = &cobra.Command{
-	Use:   "authenticate",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "prvd authenticate",
+	Short: "Authenticate using user credentials for provide.services.",
+	Long:  `Authenticate using user credentials for provide.services`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("authenticate called")
 	},
