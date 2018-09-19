0f8102cb0bde975883152a6cfaebd7f436ba55bf
Kevin Munc <muncman@gmail.com>
2018-09-19 01:39:55 -0400

    Add support for wallet creation to dapp init.
    - Including flag to opt-out of wallet creation.

--- cmd/dapps_init.go
+++ cmd/dapps_init.go
@@ -12,6 +12,7 @@ import (
 
 var dappName string
 var withoutAPIToken bool
+var withoutWallet bool
 
 var dappsInitCmd = &cobra.Command{
 	Use:   "init --name 'my awesome dapp' --network 024ff1ef-7369-4dee-969c-1918c6edb5d4",
@@ -21,6 +22,10 @@ var dappsInitCmd = &cobra.Command{
 }
 
 func createApplication(cmd *cobra.Command, args []string) {
+	if withoutAPIToken && !withoutWallet {
+		fmt.Println("Cannot create an application that has a wallet but no API token.")
+		os.Exit(1)
+	}
 	token := requireAPIToken()
 	params := map[string]interface{}{
 		"name": dappName,
@@ -42,6 +47,9 @@ func createApplication(cmd *cobra.Command, args []string) {
 	if !withoutAPIToken {
 		createAPIToken(cmd, args)
 	}
+	if !withoutWallet {
+		createWallet(cmd, args)
+	}
 }
 
 func init() {
@@ -52,4 +60,5 @@ func init() {
 	dappsInitCmd.MarkFlagRequired("network")
 
 	dappsInitCmd.Flags().BoolVar(&withoutAPIToken, "without-api-token", false, "do not create a new API token for this dapp")
+	dappsInitCmd.Flags().BoolVar(&withoutWallet, "without-wallet", false, "do not create a new wallet (signing identity) for this dapp")
 }
