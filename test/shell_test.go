eaf7c78139d2524b778fc1940d0ca1f0d9a11fcf
Kyle Thomas <kyle@provide.services>
2021-05-17 04:00:52 -0400

    Add test package

---
+++ test/shell_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/provideservices/provide-cli/cmd/shell"
+	"github.com/spf13/cobra"
+)
+
+func TestShell(t *testing.T) {
+	type args struct {
+		cmd  *cobra.Command
+		args []string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "prvd wallets",
+			args: args{
+				cmd:  shell.ShellCmd,
+				args: []string{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.args.cmd.SetArgs(tt.args.args)
+			tt.args.cmd.Execute()
+			// tt.args.cmd.RunE(tt.args.cmd, tt.args.args)
+		})
+	}
+}
