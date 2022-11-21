95a7654d1c45d410ea5240701c2145c81a368d26
Kyle Thomas <kyle@provide.services>
2022-11-21 00:22:58 -0500

    Fix syntax error

--- prvd/common/baseline.go
+++ prvd/common/baseline.go
@@ -504,7 +504,7 @@ func RequireOrganizationEndpoints(fn func(), tunnelShutdownFn func(*string), api
 			BPIEndpoint = fmt.Sprintf("http://%s:%d", *publicIP, apiPort)
 		}
 
-		messagingPort == "" {
+		if messagingPort == "" {
 			MessagingEndpoint = fmt.Sprintf("nats://%s:%d", *publicIP, messagingPort)
 		}
 
