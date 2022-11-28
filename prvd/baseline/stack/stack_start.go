dee940729e7fe0c1790325d70b6b166031e095be
Kyle Thomas <kyle@provide.services>
2022-11-28 03:01:28 -0500

    Force the use of the configured redis container port during bpi stack init

--- prvd/baseline/stack/stack_start.go
+++ prvd/baseline/stack/stack_start.go
@@ -858,7 +858,7 @@ func applyFlags() {
 	// HACK
 	if strings.HasSuffix(redisHostname, "-redis") {
 		redisHostname = fmt.Sprintf("%s-redis", name)
-		redisHosts = fmt.Sprintf("%s:%d", redisHostname, redisPort)
+		redisHosts = fmt.Sprintf("%s:%d", redisHostname, redisContainerPort)
 	}
 
 	// HACK
